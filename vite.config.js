69173dcd3695440ca96df04cdd5776f79001e300
Rawat107 <vaibhavrawat0046@gmail.com>
2025-03-29 23:06:24 +0530

    Vite cofig update

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,14 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   server: {
-    port: 5173 // Development port (optional)
+    port: 5173 // Development port
   },
   preview: {
     host: '0.0.0.0',
-    port: 10000  
+    port: 10000,
+    allowedHosts: [
+        'weather-forecast-app-bs2a.onrender.com', // Render domain
+        'localhost' //local development access
+    ]
   }
 })
